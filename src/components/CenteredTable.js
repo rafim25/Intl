ebac69e26666df8fb5566a7f89469314fb336af0
Mohammad Rafee <rafim25@yahoo.com>
2023-03-08 19:11:13 +0530

    Create CenteredTable.js

---
+++ src/components/CenteredTable.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+
+const useStyles = makeStyles({
+  table: {
+    width: 400,
+    margin: "auto"
+  }
+});
+
+function createData(number, item, qty, price) {
+  return { number, item, qty, price };
+}
+
+const rows = [
+  createData(1, "Apple", 5, 3),
+  createData(2, "Orange", 2, 2),
+  createData(3, "Grapes", 3, 1),
+  createData(4, "Tomato", 2, 1.6),
+  createData(5, "Mango", 1.5, 4)
+];
+
+export default function CenteredTable() {
+  const classes = useStyles();
+
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="simple table" className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>S.No</TableCell>
+            <TableCell align="right">Item</TableCell>
+            <TableCell align="right">Quantity&nbsp;(kg)</TableCell>
+            <TableCell align="right">Price&nbsp;($)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.number}>
+              <TableCell component="th" scope="row">
+                {row.number}
+              </TableCell>
+              <TableCell align="right">{row.item}</TableCell>
+              <TableCell align="right">{row.qty}</TableCell>
+              <TableCell align="right">{row.price}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
